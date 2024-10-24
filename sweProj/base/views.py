754f186d91856ce1d2ef3b53d9086999c6b4a617
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-10-24 15:28:45 -0400

    Franks current NavBar

--- sweProj/base/views.py
+++ sweProj/base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
+def loginPage(request):
+    return render(request, 'login.html')
 
 # Create your views here.
