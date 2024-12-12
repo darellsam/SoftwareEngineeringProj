c502c134e62174f8282b5e09b7bec3ea3a13c0ea
Abel Varghese <abelv0538@gmail.com>
2024-12-12 18:15:26 -0500

    Final Final Final I swear bro

--- sweProj/base/urls.py
+++ sweProj/base/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.jobBoard, name='jobBoard'),
+    path('home/', views.home, name='home'),
     path('login/', views.loginPage, name='loginPage'),
     path('register/', views.registerPage, name='registerPage'),
     path('jobBoard/', views.jobBoard, name='jobBoard'),
